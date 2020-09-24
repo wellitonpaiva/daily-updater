27085beb4ef8cbbc811f9c4309ce9632bbf82c5e
Welliton Paiva <wellitonpaiva@gmail.com>
2020-09-25 00:05:23 +0100

    using webclient

--- src/main/java/com/wellitonpaiva/dailyupdater/CovidUpdater.java
+++ src/main/java/com/wellitonpaiva/dailyupdater/CovidUpdater.java
@@ -1,35 +1,56 @@
 package com.wellitonpaiva.dailyupdater;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
+import org.springframework.web.reactive.function.client.WebClient;
 
-import javax.net.ssl.HttpsURLConnection;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
-import java.util.zip.GZIPInputStream;
+import java.time.LocalDate;
 
 public class CovidUpdater {
 
     String apiUrl = "https://api.coronavirus.data.gov.uk/v1/data?";
-    String parameters = "filters=areaType=nation&structure={\"date\":\"date\",\"newCasesByPublishDate\":\"newCasesByPublishDate\"}&latestBy=newCasesByPublishDate";
+    String totalCasesParameters = "filters=areaType=nation&structure={" +
+            "\"date\":\"date\"" +
+            ",\"newCasesByPublishDate\":\"newCasesByPublishDate\"}" +
+            "&latestBy=newCasesByPublishDate";
 
-    int getNumberOfCases() throws IOException {
-        URL url = new URL(apiUrl + parameters);
-        HttpsURLConnection httpsURLConnection = (HttpsURLConnection) url.openConnection();
+    String totalCasesInLondonParameters = "filters=areaType=region;areaName=London&structure={" +
+            "\"date\":\"date\"" +
+            ",\"newCasesByPublishDate\":\"newCasesByPublishDate\"}" +
+            "&latestBy=newCasesByPublishDate&page=1";
 
-        try (GZIPInputStream gzipInputStream = new GZIPInputStream(httpsURLConnection.getInputStream());
-             BufferedReader reader = new BufferedReader(new InputStreamReader(gzipInputStream, StandardCharsets.UTF_8))) {
-            StringBuilder builder = new StringBuilder();
-            String input;
-            while ((input = reader.readLine()) != null) {
-                builder.append(input);
-            }
-            ObjectMapper objectMapper = new ObjectMapper();
-            CovidResponse covidData = objectMapper.readValue(builder.toString(), CovidResponse.class);
-            return covidData.getData().stream().mapToInt(CovidData::getNewCasesByPublishDate).sum();
-        }
+    Integer getTotalNumberOfCases() {
+        return WebClient.create()
+                .get()
+                .uri(getUri(totalCasesParameters))
+                .retrieve()
+                .bodyToMono(CovidResponse.class)
+                .blockOptional()
+                .orElseThrow()
+                .getData()
+                .stream()
+                .map(CovidData::getNewCasesByPublishDate)
+                .mapToInt(Integer::intValue).sum();
+    }
+
+    Integer getLondonNumberOfCases() {
+        return WebClient.create()
+                .get()
+                .uri(getUri(totalCasesInLondonParameters))
+                .retrieve()
+                .bodyToMono(CovidResponse.class)
+                .blockOptional()
+                .orElseThrow()
+                .getData()
+                .stream()
+                .map(CovidData::getNewCasesByPublishDate)
+                .mapToInt(Integer::intValue).sum();
+    }
+    @SneakyThrows
+    private String getUri(String parameters) {
+        return apiUrl + parameters
+                .replace("{", "%7B")
+                .replace("}", "%7D")
+                .replace("\"", "%22");
     }
 
 }
--- src/test/java/com/wellitonpaiva/dailyupdater/CovidUpdaterTest.java
+++ src/test/java/com/wellitonpaiva/dailyupdater/CovidUpdaterTest.java
@@ -2,17 +2,20 @@ package com.wellitonpaiva.dailyupdater;
 
 import org.junit.jupiter.api.Test;
 
-import java.net.MalformedURLException;
-import java.net.URISyntaxException;
-
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
 class CovidUpdaterTest {
 
     @Test
-    void getNumberOfCases() throws URISyntaxException, MalformedURLException {
+    void getTotalNumberOfCases() {
         CovidUpdater sut = new CovidUpdater();
-        assertThat(sut.getNumberOfCases(), is(10));
+        assertThat(sut.getTotalNumberOfCases(), is(6634));
+    }
+
+    @Test
+    void getLondonNumberOfCases() {
+        CovidUpdater sut = new CovidUpdater();
+        assertThat(sut.getLondonNumberOfCases(), is(620));
     }
 }
